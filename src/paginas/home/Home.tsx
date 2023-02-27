b4e94e4778c0956bc9716c1ebf52c4469d42904a
ChrisPCruz99 <christian.pcruz99@gmail.com>
2023-02-27 14:01:57 -0300

    Novo: criação do componente Home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css' 
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://imgur.com/H88yIo2.png" alt="Imagem de tela inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
