7fb03418b2ac77bedd4650b7e02581ac0ae7710c
ChrisPCruz99 <christian.pcruz99@gmail.com>
2023-02-28 15:44:16 -0300

    Novo: Criação do Navbar, Home, Footer e implementação do Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 import Home from './paginas/home/Home';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import { Grid } from '@material-ui/core';
 
 function App() {
   return (
-    <Home />
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
   );  
 }
 
